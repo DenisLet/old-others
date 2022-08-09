9d9412633ded563db2d48fb0482d47859bcbf3ef
DenisLet <denis25ru@gmail.com>
2022-08-09 11:54:12 +0300

    close to finish

---
+++ venv/SoccerHalfs.py
@@ -0,0 +1,98 @@
+from playwright.sync_api import sync_playwright
+from collections import Counter
+from functools import reduce
+url = "https://www.soccer24.com/match/vHM6Muh9/#/match-summary/match-summary"
+
+
+def forming(page,link):                                    # NEED ADD TYPE SPORT AND FIXABLE CSS SELECTOR
+    page.goto(link)
+    matches = page.query_selector_all("[id^='g_1']")
+    match_list = list()
+    for i in matches:
+        line = i.inner_text()
+        if "Awrd" in line:
+            continue
+        if "(0)" in line or "(1)" in line or "(2)" in line:
+            match_list.append(line.split())
+    return match_list
+
+def first_half_results(matches,loc):
+    team_scored = list()
+    team_coceded = list()
+    total_half = list()
+    if loc == "home":
+        scored,conceded = 2,3
+    else:
+        scored,conceded = 3,2
+    for i in matches:
+        scores = [j for j in i if j.isdigit() or j.isalnum() == False][-4:]
+        if "(" in scores[0]:
+            scores[0], scores[1], scores[2], scores[3] = scores[2], scores[3], scores[0], scores[1]
+        team_scored.append(int(scores[scored].replace("(", "").replace(")", "")))
+        team_coceded.append(int(scores[conceded].replace("(", "").replace(")", "")))
+    return team_scored,team_coceded                # 0 -scored 1 - conceded
+
+def separation_home_away(team_,all_matches):
+    count = 0
+    home_matches = list()
+    away_matches = list()
+    for i in all_matches:
+        count += 1
+        if len(team_) == 1:
+            if i[1] in team_:
+                home_matches.append(i)
+            elif i[2] in team_ and ":" in i[1]:
+                home_matches.append(i)
+            elif (i[1] == "AET" or i[2] == "AET" or i[1] == "Pen" or i[2] == "Pen") and i[3] in team_:
+                home_matches.append(i)
+            else:
+                away_matches.append(i)
+        else:
+            if i[1] in team_ and i[2] in team_:
+                home_matches.append(i)
+            elif i[2] in team_ and i[3] in team_ and ":" in i[1]:
+                home_matches.append(i)
+            elif (i[1] == "AET" or i[2] == "AET" or i[1] == "Pen" or i[2] == "Pen") and i[3] in team_ and i[4] in team_:
+                home_matches.append(i)
+            else:
+                away_matches.append(i)
+    return home_matches,away_matches,count
+def team_name(list):
+    if len(list) > 0:
+        team = set(reduce(lambda i, j: i & j, (set(x) for x in list)))
+    else:
+        team = ""
+    return team
+def summery(scores):
+    return [i for i in scores]
+def teams_stat(link):
+    with sync_playwright() as p:
+        browser = p.chromium.launch()
+        page = browser.new_page()
+        page.goto(url)
+        chain = url.split("/")[1:3]
+        team_home= 'https://'.join(chain)+page.query_selector_all('a.participant__participantName')[0].get_attribute(
+            "href")
+        team_away = 'https://'.join(chain)+page.query_selector_all('a.participant__participantName')[1].get_attribute(
+            "href")
+        link_hometeam_results = f'{team_home}/results/'
+        link_awayteam_resalts =f'{team_away}/results/'
+        home_name = team_name(forming(page,link_hometeam_results))
+        away_name = team_name(forming(page,link_awayteam_resalts))
+        home_results = separation_home_away(home_name,forming(page,link_hometeam_results))     # 0 - home, 1 - away
+        away_results = separation_home_away(away_name,forming(page,link_awayteam_resalts))
+        print(home_name,away_name)
+        home_team_scores_list_at_home = summery(first_half_results(home_results[0],loc="home")[0])
+        home_team_conceded_list_at_home = summery(first_half_results(home_results[0],loc="home")[1])
+        home_team_scores_list_away = summery(first_half_results(home_results[1],loc="away")[0])
+        away_team_scores_list_at_home = summery(first_half_results(away_results[0],loc="home")[0])
+        away_team_scores_list_away = summery(first_half_results(away_results[1],loc="away")[0])
+        print(home_team_scores_list_at_home)
+        print(home_team_conceded_list_at_home)
+        # print(home_team_scores_list_away)
+        # print(away_team_scores_list_at_home)
+        # print(away_team_scores_list_away)
+
+teams_stat(url)
+
+
--- venv/play.py
+++ venv/play.py
@@ -1,6 +1,6 @@
 from playwright.sync_api import sync_playwright
 from collections import Counter
-url = "https://www.soccer24.com/match/Q5x2SpTj/#/match-summary/match-summary"
+url = "https://www.soccer24.com/match/4hN78TP7/#/match-summary/match-summary"
 def teams(link):
     with sync_playwright() as p:
         browser = p.chromium.launch()
@@ -18,35 +18,42 @@ def teams(link):
         match_list, home_matches, away_matches = [], [], []
         first_half_home,fulltime_home = [],[]
         first_half_away,fulltime_away = [],[]
+        # for i in matches:
+        #     if "Awrd" in i.inner_text() or "(0)" not in i.inner_text():
+        #         continue
+        #     if len(i.inner_text()) == 0:
+        #         continue
+        #     match_list.append(i.inner_text().split())
         for i in matches:
-            # print(i.inner_text().split())
-            match_list.append(i.inner_text().split())
-        team_set = set(match_list[0]) & set(match_list[1]) & set(match_list[2])\
-                   & set(match_list[3]) & set(match_list[4])& set(match_list[5])\
-                   & set(match_list[6]) & set(match_list[7])& set(match_list[8])\
-                   & set(match_list[9]) & set(match_list[10]) & set(match_list[11])\
-                   & set(match_list[12]) & set(match_list[13])& set(match_list[14])\
-                   & set(match_list[15]) & set(match_list[16])& set(match_list[17])
-
-        print(team_set)
+            line = i.inner_text()
+            if "(0)" in line or "(1)" in line or "(2)" in line:
+                match_list.append(line.split())
+        print(len(match_list))
+        from functools import reduce
+        if len(match_list) > 0 :
+            team_ = list(reduce(lambda i, j: i & j, (set(x) for x in match_list)))
+        else:
+            team_ = ""
+        print(team_)
         count = 0
         for i in match_list:
+
             count += 1
-            if len(team_set) == 1:
-                if i[1] in team_set:
+            if len(team_) == 1:
+                if i[1] in team_:
                     home_matches.append(i)
-                elif i[2] in team_set and ":" in i[1] :
+                elif i[2] in team_ and ":" in i[1] :
                     home_matches.append(i)
-                elif (i[1] == "AET" or i[2] == "AET") and i[3] in team_set:
+                elif (i[1] == "AET" or i[2] == "AET") and i[3] in team_:
                     home_matches.append(i)
                 else:
                     away_matches.append(i)
             else:
-                if i[1] in team_set and i[2] in team_set:
+                if i[1] in team_ and i[2] in team_:
                     home_matches.append(i)
-                elif i[2] in team_set and i[3] in team_set and ":" in i[1] :
+                elif i[2] in team_ and i[3] in team_ and ":" in i[1] :
                     home_matches.append(i)
-                elif (i[1] == "AET" or i[2] == "AET") and i[3] in team_set and i[4] in team_set:
+                elif (i[1] == "AET" or i[2] == "AET") and i[3] in team_ and i[4] in team_:
                     home_matches.append(i)
                 else:
                     away_matches.append(i)
@@ -67,6 +74,7 @@ def teams(link):
         print("________________________________________________________________________________________")
         for i in away_matches:
             scores = [j for j in i if j.isdigit() or j.isalnum() == False][-4:]
+
             if "(" in scores[0]:
                 scores[0],scores[1],scores[2],scores[3] = scores[2],scores[3],scores[0],scores[1]
             print(scores)
@@ -75,6 +83,13 @@ def teams(link):
             fulltime_away.append(int(scores[0]) + int(scores[1]))
         null,one,more = 0,0,0
         for i in first_half_home:
+            if i == 0:
+                null += 1
+            elif i == 1:
+                one += 1
+            else:
+                more += 1
+        for i in first_half_away:
             if i == 0:
                 null += 1
             elif i == 1:
